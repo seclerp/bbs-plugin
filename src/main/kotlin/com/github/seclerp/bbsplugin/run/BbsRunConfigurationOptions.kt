1b3d6bf3c17012307c720da09be9fffc56b402d4
Andrii Rublov <seclerp.dev@gmail.com>
2025-02-14 18:26:15 +0100

    Make `.` a default value for a profile field

--- src/main/kotlin/com/github/seclerp/bbsplugin/run/BbsRunConfigurationOptions.kt
+++ src/main/kotlin/com/github/seclerp/bbsplugin/run/BbsRunConfigurationOptions.kt
@@ -3,7 +3,7 @@ package com.github.seclerp.bbsplugin.run
 import com.intellij.execution.configurations.RunConfigurationOptions
 
 class BbsRunConfigurationOptions : RunConfigurationOptions() {
-    var profile by string("")
+    var profile by string(".")
     var entryPoint by string("")
 
     var additionalArguments by string("")
