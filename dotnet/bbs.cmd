df9d8000e596aa8918bd2f47b3a6aa5b48535942
Andrii Rublov <seclerp.dev@gmail.com>
2025-06-03 21:35:24 +0200

    Add coloring to a mocked bbs.cmd script

--- dotnet/bbs.cmd
+++ dotnet/bbs.cmd
@@ -1,15 +1,18 @@
 @echo off
 :: Batch section for Windows
 if not "%OS%" == "" (
+    color 0A
     echo Welcome to the fake BBS script.
+    color 0E
     echo Entry point: %1
+    color 0C
     echo Profile: %2
     ping -n 6 127.0.0.1 > nul
     exit /b
 )
 
 # Bash section for Unix environments
-echo "Welcome to the fake BBS script."
-echo "Entry point: $1"
-echo "Profile: $2"
+echo -e "\033[32mWelcome to the fake BBS script.\033[0m"
+echo -e "\033[33mEntry point: $1\033[0m"
+echo -e "\033[31mProfile: $2\033[0m"
 sleep 5
